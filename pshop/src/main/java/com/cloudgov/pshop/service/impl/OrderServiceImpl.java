11a072b699cd7f350694f0c17526fb880c3f4bbd
Mr P <parthi05@gmail.com>
2025-03-27 21:18:22 +0530

    Update OrderServiceImpl.java
    reverted the changes on delete function

--- pshop/src/main/java/com/cloudgov/pshop/service/impl/OrderServiceImpl.java
+++ pshop/src/main/java/com/cloudgov/pshop/service/impl/OrderServiceImpl.java
@@ -42,8 +42,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Mono<Void> deleteOrder(String id) {
-        //return orderRepository.deleteById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found")));
-        updateStatus("cancel",id);
+        return orderRepository.deleteById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found")));
     }
 
     @Override
