acaa3d73aaccaa41a08ccc722d292f90d47bbb10
Mr P <parthi05@gmail.com>
2025-03-30 03:58:41 +0530

    Update OrderServiceImpl.java

--- pshop/src/main/java/com/cloudgov/pshop/service/impl/OrderServiceImpl.java
+++ pshop/src/main/java/com/cloudgov/pshop/service/impl/OrderServiceImpl.java
@@ -1,18 +1,25 @@
 package com.cloudgov.pshop.service.impl;
 
+import com.cloudgov.pshop.dto.OrderPizza;
 import com.cloudgov.pshop.entity.Order;
+import com.cloudgov.pshop.entity.Pizza;
 import com.cloudgov.pshop.mapper.DTOMapper;
 import com.cloudgov.pshop.repository.OrderRepository;
 import com.cloudgov.pshop.repository.PizzaRepository;
 import com.cloudgov.pshop.service.OrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.*;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -22,17 +29,20 @@ public class OrderServiceImpl implements OrderService {
 
     private PizzaRepository pizzaRepository;
 
+    private ReactiveMongoTemplate reactiveMongoTemplate;
+
     @Override
     public Mono<Order> saveOrder(Order dto) {
-        if(dto.getID()!=null) return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id not allowed"));
+        if (dto.getID() != null)
+            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id not allowed"));
         else {
-            return validatePizzas(dto.getPizzas()).flatMap((flag) ->{
-                            if(flag) {
-                                return orderRepository.save(dto).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found")));
-                            } else
-                                return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Pizza Specified!"));
-                        }
-                    );
+            return validatePizzas( dto.getPizzas()  ).flatMap((flag) -> {
+                        if (flag) {
+                            return orderRepository.save(dto).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found")));
+                        } else
+                            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Pizza Specified!"));
+                    }
+            );
         }
     }
 
@@ -48,35 +58,35 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Mono<Order> updateOrder(Order dto, String id) {
-            return  orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj)->{
-                return validatePizzas(dto.getPizzas()).flatMap((flag) ->{
-                    if(flag) {
-                        return orderRepository.save(DTOMapper.map(obj,dto));
-                    } else
-                        return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Pizza Specified!"));
+        return orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj) -> {
+            return validatePizzas( dto.getPizzas()).flatMap((flag) -> {
+                        if (flag) {
+                            return orderRepository.save(DTOMapper.map(obj, dto));
+                        } else
+                            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Pizza Specified!"));
                     }
-                    );
-            });
+            );
+        });
     }
 
     @Override
     public Mono<Void> deleteOrder(String id) {
-        return  orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj)->{
+        return orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj) -> {
             return orderRepository.deleteById(id);
         });
     }
 
     @Override
     public Mono<Order> softdeleteOrder(String id) {
-        return  orderRepository.findByIDAndStatusNot(id, "cancel").switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj)->{
-           obj.setStatus("cancel");
-           return orderRepository.save(obj);
+        return orderRepository.findByIDAndStatusNot(id, "cancel").switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj) -> {
+            obj.setStatus("cancel");
+            return orderRepository.save(obj);
         });
     }
 
     @Override
     public Mono<Order> updateStatus(String status, String id) {
-        return  orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj)->{
+        return orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj) -> {
             obj.setStatus(status);
             return orderRepository.save(obj);
         });
@@ -84,20 +94,77 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Mono<com.cloudgov.pshop.dto.Order> getOrderAggr(String id) {
-        return  orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj)->{
-            return pizzaRepository.findAllById(obj.getPizzas()).collectList().flatMap((objs) -> {
-                return Mono.just(com.cloudgov.pshop.dto.Order.builder()
-                                .ID(obj.getID())
-                                .pizzas(objs)
-                                .status(obj.getStatus())
-                                .timestamp(obj.getTimestamp())
-                        .build());
-            });
-        });
+
+        // unwind
+        UnwindOperation unwindOperation = Aggregation.unwind("$pizzas");
+//        $addFields: {
+//            userIdObjectId: { $toObjectId: "$pizzas" }
+//        }
+        AddFieldsOperation addFieldsOperation = Aggregation.addFields().addFieldWithValueOf("pizzaObjectId", Map.of("$toObjectId", "$pizzas")).build();
+
+        LookupOperation  lookupOperation = Aggregation.lookup("pizzas", "pizzaObjectId", "_id", "pizzas");
+        MatchOperation matchOperation = Aggregation.match(Criteria.where("_id").is(id));
+//        // group
+//        GroupOperation groupOperation = Aggregation.group("skills").push("name").as("names");
+        // project
+        ProjectionOperation projectionOperation = Aggregation.project("status", "timestamp", "pizza")
+                                .and("_id").as("ID")
+
+                .and("$pizza._id").as("pizzaId")
+                .and("$pizza.name").as("name")
+                .and("$pizza.description").as("description")
+                .and("$pizza.toppings").as("toppings")
+                .and("$pizza.size").as("size")
+                .and("$pizza.price").as("price")
+                ;
+
+        // add all the stages
+        TypedAggregation<Order> orderTypedAggregation = Aggregation.newAggregation(
+                Order.class,
+                matchOperation,
+                unwindOperation,
+                addFieldsOperation,
+                lookupOperation//,
+//                projectionOperation
+        );
+
+        return reactiveMongoTemplate
+                .aggregate(
+                        orderTypedAggregation,
+                        OrderPizza.class
+                ).collectList().flatMap((objs)->{
+                    if(objs.isEmpty()) return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"));
+                    else{
+                        OrderPizza orderPizza = objs.getFirst();
+                        List<Pizza> pizzas = new ArrayList<>(objs.size());
+                        for(OrderPizza op:objs){
+                            pizzas.addAll(op.getPizzas());
+                        }
+                        return Mono.just(com.cloudgov.pshop.dto.Order.builder()
+                                .ID(id)
+                                .pizzas(pizzas)
+                                .status(orderPizza.getStatus())
+                                .timestamp(orderPizza.getTimestamp())
+                                .build());
+                    }
+                });
+
+
+//        return orderRepository.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"))).flatMap((obj) -> {
+//            return pizzaRepository.findAllById(  obj.getPizzas() ).collectList().flatMap((objs) -> {
+//                return Mono.just(com.cloudgov.pshop.dto.Order.builder()
+//                        .ID(obj.getID())
+//                        .pizzas(objs)
+//                        .status(obj.getStatus())
+//                        .timestamp(obj.getTimestamp())
+//                        .build());
+//            });
+//        });
     }
 
-    Mono<Boolean> validatePizzas(List<String> ids){
-        if(ids.isEmpty()) return Mono.just(false);
-        else return pizzaRepository.findAllById(ids).collectList().flatMap((objs) -> Mono.just(ids.size() == objs.size()));
+    Mono<Boolean> validatePizzas(List<String> ids) {
+        if (ids.isEmpty()) return Mono.just(false);
+        else
+            return pizzaRepository.findAllById(ids).collectList().flatMap((objs) -> Mono.just(ids.size() == objs.size()));
     }
 }
