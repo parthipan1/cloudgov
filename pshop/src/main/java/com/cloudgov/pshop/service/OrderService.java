1aa73a4853e39a1415c6c15402ed670b338126fb
Mr P <parthi05@gmail.com>
2025-03-30 09:55:48 +0530

    Update OrderService.java
    Changed aggregate functions.

--- pshop/src/main/java/com/cloudgov/pshop/service/OrderService.java
+++ pshop/src/main/java/com/cloudgov/pshop/service/OrderService.java
@@ -2,8 +2,14 @@ package com.cloudgov.pshop.service;
 
 import com.cloudgov.pshop.entity.Order;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public interface OrderService {
 
@@ -21,5 +27,14 @@ public interface OrderService {
 
     Mono<Order> updateStatus( String status, String id);
 
-    Mono<com.cloudgov.pshop.dto.Order> getOrderAggr(@NotNull String id);
+   default  Mono<com.cloudgov.pshop.dto.Order> getOrderAggr(@NotNull String id){
+       return getOrderAggr(Collections.singletonList(id)).collectList().flatMap((list) -> {
+           if (!list.isEmpty())
+               return Mono.just(list.getFirst());
+           else
+               return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"));
+       });
+   }
+
+    Flux<com.cloudgov.pshop.dto.Order> getOrderAggr(List<String> ids);
 }
